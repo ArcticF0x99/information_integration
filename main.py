52e86fa323e1223d2097e7b0129a52b227843be0
GiulianaLavagnino95 <giulianalavagnino25@gmail.com>
2023-01-28 16:18:51 +0100

    initial commit.

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from strsimpy import NormalizedLevenshtein, Jaccard
-import numpy as np
-from typing import List, Tuple
+from Levenshtein import distance
+from jellyfish import jaro_winkler
+from nltk.metrics import jaccard_distance
+
 
 
 def function(name, developer):
@@ -15,12 +17,6 @@ if __name__ == '__main__':
 
 
 
-
-
-from strsimpy.normalized_levenshtein import NormalizedLevenshtein
-
-nl = NormalizedLevenshtein()
-
 #list of strings (ordered, items can be accessed by index)
 source1 = ["appid", "name", "developer", "publisher", "score_rank",
            "owners", "average_forever", "average_2weeks", "median_forever",
@@ -33,23 +29,57 @@ schema = {
     "platform": ["Name", "Release Date"]
 }
 
-max_distance_pairs = {}
 
-for key, value in schema.items():
-    for source in source1:
-        max_distance = 0
-        max_distance_pair = ("", "")
-        for val in value:
-            distance = nl.distance(source, val)
-            transformed_distance = 1 - distance
-            if transformed_distance > max_distance:
-                max_distance = transformed_distance
-                max_distance_pair = (source, val)
-        max_distance_pairs[source] = (max_distance, max_distance_pair)
+#looping through the values of the schema dictionary
+#each value find the closest match in source1 using Levenshtein distance
+#then add that match to a new dictionary "mapped_schema"
+def map_schemas(source1, schema):
+    mapped_schema = {}
+    #iterate through each key-value pair of the schema dictionary
+    #items() is a method that returns a list of key-value pairs (tuples) from the schema dictionary
+    for table_name, fields in schema.items():
+        mapped_fields = []
+        for field in fields:
+            closest_match = min(source1, key=lambda x: distance(x.lower(), field.lower()))
+            mapped_fields.append(closest_match)
+        mapped_schema[table_name] = mapped_fields
+    return mapped_schema
+def map_schemas(source1, schema):
+    mapped_schema = {}
+    for table_name, fields in schema.items():
+        mapped_fields = []
+        for field in fields:
+            #replace min with max function for Jaro Winkler
+            closest_match = max(source1, key=lambda x: jaro_winkler(x.lower(), field.lower()))
+            mapped_fields.append(closest_match)
+        mapped_schema[table_name] = mapped_fields
+    return mapped_schema
 
-for key, value in max_distance_pairs.items():
-    print(f"The highest transformed distance for {key} is {value[0]} for the pair {value[1]}")
+def map_schemas(source1, schema):
+    mapped_schema = {}
+    for table_name, fields in schema.items():
+        mapped_fields = []
+        for field in fields:
+            closest_match = max(source1, key=lambda x: jaccard_distance(set(x.lower()), set(field.lower())))
+            mapped_fields.append(closest_match)
+        mapped_schema[table_name] = mapped_fields
+    return mapped_schema
 
+result=map_schemas(source1, schema)
+print("Jaro Winkler :" )
+print(result)
+print("")
+
+result=map_schemas(source1, schema)
+#new dictionary with the same keys as the schema dictionary
+#values are the closest match from source1 for each value in the schema dictionary
+print("Normalized Levenshtein :" )
+print(result)
+print("")
+
+result=map_schemas(source1, schema)
+print("Jaccard distance :" )
+print(result)
 
 
 
