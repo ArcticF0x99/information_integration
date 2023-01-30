e24925d13b6d5fa25db7dc84de01cb4f8c7afaba
GiulianaLavagnino95 <giulianalavagnino25@gmail.com>
2023-01-30 19:15:09 +0100

    work on schema matching

--- main.py
+++ main.py
@@ -1,86 +1,98 @@
-from strsimpy import NormalizedLevenshtein, Jaccard
-from Levenshtein import distance
-from jellyfish import jaro_winkler
-from nltk.metrics import jaccard_distance
+from strsimpy.normalized_levenshtein import NormalizedLevenshtein
+from strsimpy.jaro_winkler import JaroWinkler
+from strsimpy.jaccard import Jaccard
 
 
-
-
-def function(name, developer):
-    print("Hello")
-
-
-if __name__ == '__main__':
-    normalized_levenshtein = NormalizedLevenshtein()
-    jaccard = Jaccard(2)
-    # print(normalized_levenshtein.distance('Name', 'Name'))
-    print(1 - jaccard.distance('Name', 'Name'))
-
-
-
-#list of strings (ordered, items can be accessed by index)
 source1 = ["appid", "name", "developer", "publisher", "score_rank",
            "owners", "average_forever", "average_2weeks", "median_forever",
            "median_2weeks", "ccu", "price", "initialprice", "discount", "tags", "languages", "genre"]
 
-#dictionary with key as string, and value as lists of string, values can be accessed by using the keys
-schema = {
-    "game": ["Name", "Publisher", "Genre", "Language"],
-    "platform_release": ["Platform", "Price", "Date", "Sales Count"],
-    "platform": ["Name", "Release Date"]
-}
+
+source2 = ["platform/hardware", "release_date", "developer", "publisher", "genre", "theme", "freanchise",
+"features", "multiplayer_types", "franchises"]
+
+schema = ["Name", "Publisher", "Genre", "Language", "Platform",
+          "Price", "Date", "Sales Count", "Name", "Release Date"]
+
+normalized_levensthein = NormalizedLevenshtein()
+jaro_winkler = JaroWinkler()
+
+def levenshtein_similarity(schema, data_source):
+    best_matches = []
+    best_scores = []
+    for column in schema:
+        max_similarity = 0
+        closest_match = ''
+
+        for schema_column in data_source:
+            similarity_lev = normalized_levensthein.similarity(column, schema_column)
+            similarity_jaro = jaro_winkler.similarity(column, schema_column)
+            if similarity > max_similarity:
+                max_similarity = similarity
+                closest_match = schema_column
+
+        best_matches.append(closest_match)
+        best_scores.append(max_similarity)
+    return best_matches, best_scores
+
+def jaro_winkler_similarity(schema, data_source):
+    max_similarity = 0
+    closest_column = ''
+    closest_match = ''
+    for column in data_source:
+        for schema_column in schema:
+            similarity = jellyfish.jaro_winkler(column, schema_column)
+            if similarity > max_similarity:
+                max_similarity = similarity
+                closest_column = schema_column
+                closest_match = column
+    return (closest_match, closest_column, similarity)
+
+def jaccard_similarity(schema, data_source):
+    max_similarity = 0
+    closest_column = ''
+    closest_match = ''
+    for column in data_source:
+        for schema_column in schema:
+            similarity = len(set(column).intersection(schema_column)) / len(set(column).union(schema_column))
+            if similarity > max_similarity:
+                max_similarity = similarity
+                closest_column = schema_column
+                closest_match = column
+    return (closest_match, closest_column, similarity)
 
 
-#looping through the values of the schema dictionary
-#each value find the closest match in source1 using Levenshtein distance
-#then add that match to a new dictionary "mapped_schema"
-def map_schemas(source1, schema):
-    mapped_schema = {}
-    #iterate through each key-value pair of the schema dictionary
-    #items() is a method that returns a list of key-value pairs (tuples) from the schema dictionary
-    for table_name, fields in schema.items():
-        mapped_fields = []
-        for field in fields:
-            closest_match = min(source1, key=lambda x: distance(x.lower(), field.lower()))
-            mapped_fields.append(closest_match)
-        mapped_schema[table_name] = mapped_fields
-    return mapped_schema
-def map_schemas(source1, schema):
-    mapped_schema = {}
-    for table_name, fields in schema.items():
-        mapped_fields = []
-        for field in fields:
-            #replace min with max function for Jaro Winkler
-            closest_match = max(source1, key=lambda x: jaro_winkler(x.lower(), field.lower()))
-            mapped_fields.append(closest_match)
-        mapped_schema[table_name] = mapped_fields
-    return mapped_schema
 
-def map_schemas(source1, schema):
-    mapped_schema = {}
-    for table_name, fields in schema.items():
-        mapped_fields = []
-        for field in fields:
-            closest_match = max(source1, key=lambda x: jaccard_distance(set(x.lower()), set(field.lower())))
-            mapped_fields.append(closest_match)
-        mapped_schema[table_name] = mapped_fields
-    return mapped_schema
+def get_best_match(source1, schema, source2):
+    levenshtein_best_match, schema_column1, levenshtein_score = levenshtein_similarity(source1, schema)
+    jaro_winkler_best_match, schema_column2, jaro_winkler_score = jaro_winkler_similarity(source1, schema)
+    jaccard_best_match, schema_column3, jaccard_score = jaccard_similarity(source1, schema)
 
-result=map_schemas(source1, schema)
-print("Jaro Winkler :" )
-print(result)
-print("")
 
-result=map_schemas(source1, schema)
-#new dictionary with the same keys as the schema dictionary
-#values are the closest match from source1 for each value in the schema dictionary
-print("Normalized Levenshtein :" )
-print(result)
-print("")
+    best_match = None
+    best_score = 0
+    if levenshtein_score > best_score:
+        best_match = levenshtein_best_match
+        best_score = levenshtein_score
+    if jaro_winkler_score > levenshtein_score:
+        best_match = jaro_winkler_best_match
+        best_score = jaro_winkler_score
+    if jaccard_score > jaccard_score:
+        best_match = jaccard_best_match
+        best_score = jaccard_score
 
-result=map_schemas(source1, schema)
-print("Jaccard distance :" )
-print(result)
+
+    return best_match, best_score
+
+
+if __name__ == '__main__':
+  """result = get_best_match(source1, schema, source2)
+  print(result)"""
+  best_matches1, best_score1 = levenshtein_similarity(schema, source1)
+  best_matches2, best_score2 = levenshtein_similarity(schema, source2)
+  print("hello")
+
+
 
 
 
